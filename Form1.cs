0265638649e9fe9f6fa6d858dbcbee32581bfb38
mariiaburiakivska <165211595+mariiaburiakivska@users.noreply.github.com>
2024-12-18 19:20:38 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace MoneyWF
+{
+    public partial class Form1 : Form
+    {
+        private MoneyContainer container = new MoneyContainer();
+
+        private void UpdateTextBoxOutPut()
+        {
+            textBoxOutput.Clear();
+
+            foreach (var money in container.GetAllMoney())
+            {
+                textBoxOutput.AppendText(money.ToString() + Environment.NewLine + Environment.NewLine);
+            }
+        }
+
+        private void ClearInputFields()
+        {
+            textBoxName.Clear();
+            textBoxAmount.Clear();
+            textBoxRate.Clear();
+            textBoxTime.Clear();
+            textBoxBonusRate.Clear();
+            textBoxSelectedName.Clear();
+            textBoxAmountChange.Clear();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void buttonAddDeposit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = textBoxName.Text;
+                double amount = double.Parse(textBoxAmount.Text);
+                double rate = double.Parse(textBoxRate.Text);
+                int time = int.Parse(textBoxTime.Text);
+
+                container.AddMoney(new Deposit(name, amount, rate, time));
+
+                UpdateTextBoxOutPut();
+                ClearInputFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Input Error");
+            }
+        }
+
+        private void buttonAddSpecialDeposit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = textBoxName.Text;
+                double amount = double.Parse(textBoxAmount.Text);
+                double rate = double.Parse(textBoxRate.Text);
+                int time = int.Parse(textBoxTime.Text);
+                double bonusRate = double.Parse(textBoxBonusRate.Text);
+
+                container.AddMoney(new SpecialDeposit(name, amount, rate, time, bonusRate));
+
+                UpdateTextBoxOutPut();
+                ClearInputFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Input Error");
+            }
+        }
+
+        private void buttonAddAmount_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = textBoxSelectedName.Text;
+                double amountToAdd = double.Parse(textBoxAmountChange.Text);
+
+                foreach (var money in container.GetAllMoney())
+                {
+                    if (money is MoneyManagement mm && mm.Name == name)
+                    {
+                        mm.AddAmount(amountToAdd);
+                        UpdateTextBoxOutPut();
+                        ClearInputFields();
+                        return;
+                    }
+
+                }
+
+                MessageBox.Show("No matching MoneyManagement object found", "Selection Error");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error{ex.Message}", "Input Error");
+            }
+        }
+
+        private void buttonWithdrawAmount_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = textBoxSelectedName.Text;
+                double amountToWithdraw = double.Parse(textBoxAmountChange.Text);
+
+                foreach (var money in container.GetAllMoney())
+                {
+                    if (money is MoneyManagement mm && mm.Name == name)
+                    {
+                        mm.WithdrawAmount(amountToWithdraw);
+                        UpdateTextBoxOutPut();
+                        ClearInputFields();
+                        return;
+                    }
+                }
+
+                MessageBox.Show("No matching MoneyManagement object found", "Selection Error");
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Not enough money");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Input Error");
+            }
+        }
+
+        private void buttonMoneyManagment_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = textBoxName.Text;
+                double amount = double.Parse(textBoxAmount.Text);
+                double rate = double.Parse(textBoxRate.Text);
+                int time = int.Parse(textBoxTime.Text);
+
+                container.AddMoney(new MoneyManagement(name, amount, rate, time));
+
+                UpdateTextBoxOutPut();
+                ClearInputFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Input Error");
+            }
+        }
+
+        private void buttonShowAll_Click(object sender, EventArgs e)
+        {
+            textBoxOutput.Clear();
+
+            foreach (var money in container.GetAllMoney())
+            {
+                textBoxOutput.AppendText(money.ToString() + Environment.NewLine + Environment.NewLine);
+            }
+
+            if (container.GetAllMoney().Count == 0)
+            {
+                MessageBox.Show("No deposits to show", "Information");
+            }
+        }
+
+        private void buttonClearAll_Click(object sender, EventArgs e)
+        {
+            container.ClearAll();
+            textBoxOutput.Clear();
+        }
+
+        private void buttonTotalAmount_Click(object sender, EventArgs e)
+        {
+            double total = container.CalculateTotal();
+            labelTotalAmount.Text = $"Total amount: {total:C}";
+        }
+
+    }
+}
---
+++ Money.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyWF
+{
+    public abstract class Money
+    {
+        public string Name { get; set; }
+        public double InitialAmount { get; set; }
+        public double Rate { get; set; }
+        public int Time { get; set; }
+
+        protected Money(string name, double initialAmount, double rate, int time)
+        {
+            Name = name;
+            InitialAmount = initialAmount;
+            Rate = rate;
+            Time = time;
+        }
+
+        public abstract double CalculateTotalAmount();
+
+        public override string ToString()
+        {
+            return $"Name: {Name}{Environment.NewLine}" + 
+                $"Amount: {InitialAmount:C}{Environment.NewLine}" + 
+                $"Rate: {Rate}%{Environment.NewLine}" + 
+                $"Time: {Time} years{Environment.NewLine}" + 
+                $"Total: {CalculateTotalAmount():C}";
+        }
+    }
+
+
+    public class Deposit : Money
+    {
+        public Deposit(string name, double initialAmount, double rate, int time)
+            : base(name, initialAmount, rate, time) { }
+
+        public override double CalculateTotalAmount()
+        {
+            return InitialAmount * Math.Pow(1 + Rate / 100, Time);
+        }
+    }
+
+
+
+    public class SpecialDeposit : Money
+    {
+        public double BonusRate { get; set; }
+
+        public SpecialDeposit(string name, double initialAmount, double rate, int time, double bonusRate)
+            : base(name, initialAmount, rate, time)
+        {
+            BonusRate = bonusRate;
+        }
+
+        public override double CalculateTotalAmount()
+        {
+            double baseAmount = InitialAmount * Math.Pow(1 + Rate / 100, Time);
+
+            if (Time > 5)
+            {
+                baseAmount += InitialAmount * (BonusRate / 100);
+            }
+
+            return baseAmount;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"Bonus Rate: {BonusRate}%{Environment.NewLine}";
+        }
+    }
+
+
+    public class MoneyManagement : Money
+    {
+        public MoneyManagement(string name, double initialAmount, double rate, int time)
+            : base(name, initialAmount, rate, time) { }
+
+        public void AddAmount(double amount)
+        {
+            InitialAmount += amount;
+        }
+
+        public void WithdrawAmount(double amount)
+        {
+            if (amount > InitialAmount)
+                throw new InvalidOperationException("Not enough money");
+            InitialAmount -= amount;
+        }
+
+        public override double CalculateTotalAmount()
+        {
+            return InitialAmount * Math.Pow(1 + Rate / 100, Time);
+        }
+    }
+
+
+    public class MoneyContainer
+    {
+        private List<Money> moneyList = new List<Money>();
+
+        public void AddMoney(Money money)
+        {
+            moneyList.Add(money);
+        }
+
+        public void ClearAll()
+        {
+            moneyList.Clear();
+        }
+
+        public List<Money> GetAllMoney()
+        {
+            return moneyList;
+        }
+
+        public double CalculateTotal()
+        {
+            return moneyList.Sum(m => m.CalculateTotalAmount());
+        }
+
+    }
+}
